414316acc045243b885fbfa012fabffa8e4ae50d
SergeBury <79988040+SergeBury@users.noreply.github.com>
2021-05-15 15:43:31 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div>
+      <div className="App">
+        <h1>Hello All</h1>
+        <h2>This is my first React app!</h2>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
